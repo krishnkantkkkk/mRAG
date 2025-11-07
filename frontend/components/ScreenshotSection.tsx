3b1c9e3ccf9b2be0b6ac82b1dc9a298bcdbb8578
Krishn Kant Kumar <krishnkantkumar8@gmail.com>
2025-11-07 10:53:55 +0530

    changed the screenshot

--- frontend/components/ScreenshotSection.tsx
+++ frontend/components/ScreenshotSection.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import screenshotImage from '../assets/screenshot.png';
 
 const ScreenshotSection: React.FC = () => {
   return (
     <section className="relative z-20 -mt-20 w-full px-4 md:px-8 bg-gradient-to-b to-gray-50 from-white">
       <div className="w-[90%] mx-auto rounded-lg shadow-2xl overflow-hidden">
         <img
-          src={screenshotImage}
+          src="../assets/screenshot.png"
           className="w-full h-auto object-cover"
         />
       </div>
